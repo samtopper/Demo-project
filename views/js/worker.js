71f1b1eb41779e5ccf8c1098252e0a505f2114f0
samtopper <sakhancse@gmail.com>
2016-11-24 17:34:45 +0530

    pushed to master branch.

---
+++ views/js/worker.js
@@ -0,0 +1,12 @@
+self.onMessage = function(e) {
+
+    try {
+        for (var i = 0; i < items.length; i++) {
+        var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+        items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+        }
+    }
+    catch(e) {
+
+    }
+};
